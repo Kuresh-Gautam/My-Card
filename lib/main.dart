900e1b71963cec6dc572f04d1d331913120aa031
Kuresh Gautam <gautamkulashekhar@gmail.com>
2022-01-03 16:46:58 +0545

    commit 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,84 +18,114 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-          CircleAvatar(
-            radius: 70,
-            backgroundImage: AssetImage("assets/kng.jpg"),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Kuresh Gautam",
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-                decoration: TextDecoration.none),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Flutter Developer",
-            style: TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
-                decoration: TextDecoration.none),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            alignment: Alignment.center,
-            height: 30,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(50),
-                    bottomLeft: Radius.circular(50))),
-            child: Text(
-              'gautamkulashekhar@gmail.com',
-              style: TextStyle(
-                  decoration: TextDecoration.none,
-                  color: Colors.black,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w700),
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.share),
+        backgroundColor: Colors.amber,
+        onPressed: () {},
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.teal,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 100,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            alignment: Alignment.center,
-            height: 30,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50))),
-            child: Text(
-              'çontact : 9860079934',
-              style: TextStyle(
-                  decoration: TextDecoration.none,
-                  color: Colors.black,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w700),
+            CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage("assets/kng.jpg"),
             ),
-          ),
-          SizedBox(
-            height: 100,
-          ),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Kuresh Gautam",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Flutter Developer",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                  decoration: TextDecoration.none),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 30,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(50),
+                      bottomLeft: Radius.circular(50))),
+              child: Text(
+                'gautamkulashekhar@gmail.com',
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 30,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50))),
+              child: Text(
+                'contact : 9860079934',
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Colors.lightBlue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(
+              Icons.person,
+              size: 40,
+            ),
+            SizedBox(
+              width: 40,
+            ),
+            Icon(
+              Icons.email_outlined,
+              size: 40,
+            ),
+          ],
+        ),
       ),
     );
   }
